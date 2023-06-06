2c9d5c44817478d9b4372267de5a4cd89584c914
Mahamadou-Nouridine <98597449+Mahamadou-Nouridine@users.noreply.github.com>
2023-06-06 22:48:45 +0100

    first commit

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,61 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import { FaTrash } from "react-icons/fa";
+import { AiFillEdit } from "react-icons/ai";
+import styles from "../styles/TodoItem.module.css";
+import { useAuthContext } from "../context/AuthContext";
+const TodoItem = ({ itemProp, handleChange, delTodo, setUpdate }) => {
+    const { user } = useAuthContext();
+  const [editing, setEditing] = useState(false);
+
+  const handleEditing = () => {
+    setEditing(true);
+  };
+
+  let viewMode = {};
+  let editMode = {};
+  if (editing) {
+    viewMode.display = "none";
+  } else {
+    editMode.display = "none";
+  }
+
+  const handleUpdatedDone = (event) => {
+    if (event.key === "Enter") {
+      setEditing(false);
+    }
+  };
+
+  return (
+    <li className={styles.item}>
+      <div className={styles.content} style={viewMode}>
+        <input
+          type="checkbox"
+          checked={itemProp.completed}
+          onChange={() => handleChange(itemProp.id)}
+        />
+        {user && (
+          <button onClick={handleEditing}>
+            <AiFillEdit
+              style={{ color: '#5e5e5e', fontSize: '16px' }}
+            />
+          </button>
+        )}
+        <button onClick={() => delTodo(itemProp.id)}>
+          <FaTrash style={{ color: "#5e5e5e", fontSize: "16px" }} />
+        </button>
+
+        {itemProp.title}
+      </div>
+      <input
+        type="text"
+        value={itemProp.title}
+        className={styles.textInput}
+        style={editMode}
+        onChange={(e) => setUpdate(e.target.value, itemProp.id)}
+        onKeyDown={handleUpdatedDone}
+      />
+    </li>
+  );
+};
+export default TodoItem;
---
+++ src/components/TodosLogic.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import TodosList from "./TodosList";
+import InputTodo from "./InputTodo";
+import { v4 as uuidv4 } from "uuid";
+// other imported components here
+const TodosLogic = () => {
+  const [todos, setTodos] = useState(getInitialTodos());
+
+  function getInitialTodos() {
+    // getting stored items
+    const temp = localStorage.getItem('todos');
+    const savedTodos = JSON.parse(temp);
+    return savedTodos || [];
+  }
+
+  useEffect(() => {
+    // storing todos items
+    const temp = JSON.stringify(todos);
+    localStorage.setItem('todos', temp);
+  }, [todos]);
+
+  const handleChange = (id) => {
+    setTodos((prevState) =>
+      prevState.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      })
+    );
+  };
+
+  const delTodo = (id) => {
+    setTodos([
+      ...todos.filter((todo) => {
+        return todo.id !== id;
+      }),
+    ]);
+  };
+
+  const addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const setUpdate = (updatedTitle, id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.title = updatedTitle;
+        }
+        return todo;
+      })
+    );
+  };
+  
+  
+
+  return (
+    <div>
+      <InputTodo addTodoItem={addTodoItem} />
+      <TodosList
+        setUpdate={setUpdate}
+        todosProps={todos}
+        handleChange={handleChange}
+        delTodo={delTodo}
+      />
+    </div>
+  );
+};
+export default TodosLogic;
